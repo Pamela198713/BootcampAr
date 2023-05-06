339f9df34458ef41c2d34c23d6f06ba885a1fe15
lauranatalial <lauranatalialasloveron@gmail.com>
2023-05-05 21:21:42 -0300

    Add files via upload

---
+++ backend/Curso.py
@@ -0,0 +1,9 @@
+class Curso:
+  def __init__ (self, titulo,  precio, descripcion, duracion, nivel, imagenes, lenguaje):
+      self.titulo = titulo
+      self.precio =precio
+      self.descripcion = descripcion
+      self.duracion = duracion 
+      self.nivel = nivel
+      self.imagenes = imágenes
+      self.lengujae = lenguaje 
