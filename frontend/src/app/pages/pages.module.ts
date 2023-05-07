c6fd6e4800562ba29e669c51ff0ba2fbed0ba4da
Pamela Sol Pilotti <pamelasol13@hotmail.com>
2023-05-07 10:07:51 -0300

    pagina de perfil

--- frontend/src/app/pages/pages.module.ts
+++ frontend/src/app/pages/pages.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
+import { PerfilComponent } from './perfil/perfil.component';
 
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    PerfilComponent
   ],
   imports: [
     CommonModule
