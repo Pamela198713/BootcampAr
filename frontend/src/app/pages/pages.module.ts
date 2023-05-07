7b85e54930f7fdd333558681f925540edfeaed07
LeonardoEC <LeonardoDanielAV@proton.me>
2023-05-06 21:48:33 -0300

    CreateCourse

--- frontend/src/app/pages/pages.module.ts
+++ frontend/src/app/pages/pages.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { CreateCoursesComponent } from './create-courses/create-courses.component';
 
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    CreateCoursesComponent
   ],
   imports: [
     CommonModule
