582abfac980c15effa902669972ee4421f385465
lidioguedez <112365426+lidioguedez@users.noreply.github.com>
2023-05-07 22:19:48 -0300

    Modificando modulos para layout y shared

--- frontend/src/app/pages/pages.module.ts
+++ frontend/src/app/pages/pages.module.ts
@@ -6,20 +6,21 @@ import { LoginComponent } from './login/login.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { SharedModule } from '../shared/shared.module';
 import { CreateCoursesComponent } from './create-courses/create-courses.component';
-
+import { LayoutModule } from '../layout/layout.module';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
     LoginComponent,
-    ProductDetailsComponent
-    PerfilComponent
+    ProductDetailsComponent,
+    PerfilComponent,
     CreateCoursesComponent
   ],
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    LayoutModule
   ]
 })
 export class PagesModule { }
