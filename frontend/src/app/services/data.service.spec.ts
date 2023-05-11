d7f57da391c5f43d08324a44f6ef7a4825d17176
lidioguedez <112365426+lidioguedez@users.noreply.github.com>
2023-05-11 15:09:14 -0300

    Modificacion de nombre del Servicio

--- frontend/src/app/services/data.service.spec.ts
+++ frontend/src/app/services/data.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { DataserviceService } from './dataservice.service';
+import { DataService } from './data.service';
 
 describe('DataserviceService', () => {
-  let service: DataserviceService;
+  let service: DataService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(DataserviceService);
+    service = TestBed.inject(DataService);
   });
 
   it('should be created', () => {
--- frontend/src/app/services/data.service.ts
+++ frontend/src/app/services/data.service.ts
@@ -4,7 +4,7 @@ import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
-export class DataserviceService {
+export class DataService {
   private apiUrl = 'http://localhost:3000';
 
   constructor(private http: HttpClient) { }
