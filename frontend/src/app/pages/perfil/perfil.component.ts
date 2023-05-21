c7b80cba36c54225f5dd46874438e979bb0f7d29
LuciaL152 <leonettil152@gmail.com>
2023-05-21 14:35:18 -0300

    product details cambio

--- frontend/src/app/pages/perfil/perfil.component.ts
+++ frontend/src/app/pages/perfil/perfil.component.ts
@@ -10,8 +10,7 @@ import { ActivatedRoute } from '@angular/router';
 })
 
 export class PerfilComponent implements OnInit {
-  datoPerfil={id: "", Nombre: "",Apellido: "",Pais: "",Email: "",Contraseña: "",Rol: "",Telefono: "",Direccion: "",Foto: "",Perfil: ""}
-  datoPerfil2=[{id: "", Nombre: "",Apellido: "",Pais: "",Email: "",Contraseña: "",Rol: "",Telefono: "",Direccion: "",Foto: "",Perfil: ""}]
+ 
  
   constructor(
     private route: ActivatedRoute,
@@ -19,20 +18,7 @@ export class PerfilComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    const idParam = this.route.snapshot.paramMap.get('id');
-    if (idParam){
-    const id = parseInt(idParam, 10);
-    this.dataService.getById('perfil',id ).subscribe({
-      next: (data) => {
-      this.datoPerfil = data;
-      console.log(data);
-     }});
-    };
-
-     this.dataService.getAll('perfil').subscribe({
-      next: (data2) => {
-      this.datoPerfil2 = data2;
-    }});
+    
   }
 
 
--- frontend/src/app/pages/product-details/product-details.component.ts
+++ frontend/src/app/pages/product-details/product-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import {DataService} from '../../services/data.service';
+import { Curso } from 'src/app/shared/interfaces/Curso';
 
 
 @Component({
@@ -9,28 +10,25 @@ import {DataService} from '../../services/data.service';
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent implements OnInit {
-  dato={id: "", Titulo: "",Precio: 0,Descripcion: "",Duracion: 0,Nivel: "",Lenguajes: "",Imagenes: ""}
-  dato2=[{id: "", Titulo: "",Precio: 0,Descripcion: "",Duracion: 0,Nivel: "",Lenguajes: "",Imagenes: ""}]
-   
-   constructor(
-      private route: ActivatedRoute,
-      private dataService: DataService
-    ) {}
-  
-  
-    ngOnInit(): void {
-      const idParam = this.route.snapshot.paramMap.get('id');
-      if (idParam) {
-        const id = parseInt(idParam, 10);
-          this.dataService.getById('curso',id).subscribe({
-            next: (data) => {
-            this.dato = data;
-            console.log(data)
-          }});
+  curso: Curso = {} as Curso;
+  constructor(
+    private route: ActivatedRoute,
+    private dataService: DataService
+  ) {}
+
+  ngOnInit(): void {
+    const idParam = this.route.snapshot.paramMap.get('id');
+    if (idParam) {
+      const id = parseInt(idParam, 10);
+      this.dataService.getById("Curso",id).subscribe({
+        next:(data) => {
+          this.curso = data
+          console.log(data);
         }
-        this.dataService.getAll('curso').subscribe({
-          next: (data2) => {
-          this.dato2 = data2;
-        }});
-    }   
-    }
+  });
+};
+}
+}
+ 
+   
+  
