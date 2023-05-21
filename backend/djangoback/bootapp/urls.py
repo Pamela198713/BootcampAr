fac2b72f9967f0555df662912d95262ec364827b
LeonardoEC <LeonardoDanielAV@proton.me>
2023-05-21 16:13:17 -0300

    Arreglo de rutas django

--- backend/djangoback/bootapp/urls.py
+++ backend/djangoback/bootapp/urls.py
@@ -4,13 +4,12 @@ from django.contrib.auth.views import LoginView, LogoutView
 from rest_framework import routers
 from .api import CursoViewSet
 
-#urlpatterns = [
-#    path('', views.register, name='register'),
-#    path('feed/', views.feed, name='feed'),
-#    path('register/', views.register, name='register'),
-#    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
-#    path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout')
-#]
+urlpatterns = [
+    path('feed/', views.feed, name='feed'),
+    path('register/', views.register, name='register'),
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout')
+]
 
 router = routers.DefaultRouter()
 
