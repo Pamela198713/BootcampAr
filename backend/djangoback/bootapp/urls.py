82b77b98428a525bd0bc72e73105ca3618d61283
lidioguedez <112365426+lidioguedez@users.noreply.github.com>
2023-05-25 13:05:58 -0300

    Creando vistas y endpoint

--- backend/djangoback/bootapp/urls.py
+++ backend/djangoback/bootapp/urls.py
@@ -1,19 +1,20 @@
 from django.urls import path
 from . import views
-from django.contrib.auth.views import LoginView, LogoutView
+from dj_rest_auth.views import LoginView, LogoutView
+from dj_rest_auth.registration.views import RegisterView
 from rest_framework import routers
-from .api import CursoViewSet
+from .views.product_create import ProductCreate
 
 urlpatterns = [
-    path('feed/', views.feed, name='feed'),
-    path('register/', views.register, name='register'),
-    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout')
+    path('auth/register/', RegisterView.as_view(), name='account_register'),
+    path('auth/login/', LoginView.as_view(), name='account_login'),
+    path('auth/logout/', LogoutView.as_view(), name='account_logout'),
+    path('product/create/', ProductCreate.as_view(), name='product_create'),
 ]
 
 router = routers.DefaultRouter()
 
-router.register('api/bootapp', CursoViewSet , 'bootapp')
+# router.register('api/bootapp', CursoViewSet , 'bootapp')
 
 urlpatterns = router.urls
 
