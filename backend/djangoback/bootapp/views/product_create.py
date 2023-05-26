e4f9b8a713947976c713f0dd38ba3320e6ed237a
lidioguedez <112365426+lidioguedez@users.noreply.github.com>
2023-05-26 00:17:47 -0300

    Modificando tablas

--- backend/djangoback/bootapp/views/product_create.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import generics, permissions
-from ..models import Product
-from ..serializers import CursoSerializer
-
-class ProductCreate(generics.CreateAPIView):
-    queryset = Product.objects.all()
-    serializer_class = CursoSerializer
-    permission_classes = [permissions.IsAuthenticated]
