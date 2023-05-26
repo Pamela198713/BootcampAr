11e19a2812f17381046902cb5a5d02bf890e7608
lidioguedez <112365426+lidioguedez@users.noreply.github.com>
2023-05-26 00:18:42 -0300

    Agregando y organizando urls

--- backend/djangoback/bootapp/urls.py
+++ backend/djangoback/bootapp/urls.py
@@ -1,20 +1,22 @@
-from django.urls import path
+"""
 from . import views
+"""
 from dj_rest_auth.views import LoginView, LogoutView
 from dj_rest_auth.registration.views import RegisterView
+from django.urls import path, include
+from .apiviews.product import CursoViewSet
 from rest_framework import routers
-from .views.product_create import ProductCreate
+
 
 urlpatterns = [
-    path('auth/register/', RegisterView.as_view(), name='account_register'),
-    path('auth/login/', LoginView.as_view(), name='account_login'),
-    path('auth/logout/', LogoutView.as_view(), name='account_logout'),
-    path('product/create/', ProductCreate.as_view(), name='product_create'),
+    path('auth/register', RegisterView.as_view(), name='account_register'),
+    path('auth/login', LoginView.as_view(), name='account_login'),
+    path('auth/logout', LogoutView.as_view(), name='account_logout'),
+    path('auth/', include('dj_rest_auth.urls')),
+    path('auth/registration', include('dj_rest_auth.registration.urls')),
+    path('api/product', CursoViewSet.as_view({'get': 'list', 'post': 'create'}), name='product-list'),
+    path('api/product/<int:pk>', CursoViewSet.as_view({'get': 'retrieve', 'put': 'update', 'patch': 'partial_update', 'delete': 'destroy'}), name='product-detail'),
 ]
 
-router = routers.DefaultRouter()
 
-# router.register('api/bootapp', CursoViewSet , 'bootapp')
-
-urlpatterns = router.urls
 
