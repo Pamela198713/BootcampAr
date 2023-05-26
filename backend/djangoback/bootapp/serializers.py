8557b989225134abcc61e8125cbbe22a33cd1140
lidioguedez <112365426+lidioguedez@users.noreply.github.com>
2023-05-26 00:19:13 -0300

    Modificando clase Serializer para add Categorias

--- backend/djangoback/bootapp/serializers.py
+++ backend/djangoback/bootapp/serializers.py
@@ -1,7 +1,17 @@
 from rest_framework import serializers
-from .models import Curso
+from .models import Curso, Categoria, CursoCategoria
 
 class CursoSerializer(serializers.ModelSerializer):
+    #, write_only=True
+    categorias = serializers.PrimaryKeyRelatedField(queryset=Categoria.objects.all(), many=True)
+  
     class Meta: 
         model = Curso 
         fields = ['id', 'titulo', 'precio', 'descripcion', 'duracion', 'nivel', 'lenguajes', 'imagenes', 'categorias']
+
+    def create(self, validated_data):
+        categorias = validated_data.pop('categorias')
+        curso = Curso.objects.create(**validated_data)
+        for categoria in categorias:
+            CursoCategoria.objects.create(curso=curso, categoria=categoria)
+        return curso
