6b3a2104b32abf1cf731be4cff65187741ae27d8
lidioguedez <112365426+lidioguedez@users.noreply.github.com>
2023-05-26 22:06:05 -0300

    Configurando SIMPLE JWT

--- backend/djangoback/bootapp/urls.py
+++ backend/djangoback/bootapp/urls.py
@@ -6,15 +6,18 @@ from dj_rest_auth.registration.views import RegisterView
 from django.urls import path, include
 from .apiviews.product import CursoViewSet
 from .apiviews.usuario import UsuarioViewSet
-from rest_framework import routers
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 
 urlpatterns = [
     path('auth/register', RegisterView.as_view(), name='account_register'),
-    path('auth/login', LoginView.as_view(), name='account_login'),
+    # path('auth/login', LoginView.as_view(), name='account_login'),
     path('auth/logout', LogoutView.as_view(), name='account_logout'),
-    path('auth/', include('dj_rest_auth.urls')),
-    path('auth/registration', include('dj_rest_auth.registration.urls')),
+    path('api/token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/usuario', UsuarioViewSet.as_view({'get': 'list', 'post': 'create'}), name='usuario'),
     path('api/usuario/<int:pk>', CursoViewSet.as_view({'get': 'retrieve', 'put': 'update', 'patch': 'partial_update', 'delete': 'destroy'}), name='usuario-detail'),
     path('api/product', CursoViewSet.as_view({'get': 'list', 'post': 'create'}), name='product-list'),
