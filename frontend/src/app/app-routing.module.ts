73f5baed10c66ad02d18918d37c511652fd4c4a0
LeonardoEC <LeonardoDanielAV@proton.me>
2023-05-28 18:23:13 -0300

    Cambiando a rutas de api

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -6,12 +6,14 @@ import { HomeComponent } from './pages/home/home.component';
 import { CreateCoursesComponent } from './pages/create-courses/create-courses.component';
 import { ProductDetailsComponent } from './pages/product-details/product-details.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { RegistroComponent } from './pages/registro/registro.component';
 
 const routes: Routes = [
   { path: '', pathMatch:'full', redirectTo:'home'}, 
   { path: 'home', component: HomeComponent },
   { path: 'createCourse', component: CreateCoursesComponent},
   { path: 'login', component: LoginComponent },
+  { path: 'register', component:RegistroComponent },
   { path: 'perfil/:id', component: PerfilComponent },
   { path: 'product-details/:id', component: ProductDetailsComponent },
   { path: 'dashboard', component: DashboardComponent },
