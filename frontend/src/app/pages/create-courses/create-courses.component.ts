707470add39c8c3dfc650eeba957287ee3985a8f
lidioguedez <112365426+lidioguedez@users.noreply.github.com>
2023-06-13 09:22:42 -0300

    Implementando la creacion de cursos

--- frontend/src/app/pages/create-courses/create-courses.component.ts
+++ frontend/src/app/pages/create-courses/create-courses.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
+import { Curso } from 'src/app/shared/interfaces/Curso';
+import { ProductsService } from '../../services/products.service';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-create-courses',
@@ -8,10 +11,13 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./create-courses.component.css']
 })
 export class CreateCoursesComponent {
-  
-  constructor(private formBuilder: FormBuilder) {
-  }
-  
+  product?: Curso ;
+  constructor(private formBuilder: FormBuilder,
+              private productsService: ProductsService,
+              private router: Router,
+              private toastr: ToastrService
+             ){}
+
     cursoForm = this.formBuilder.group({  
     titulo: ['', Validators.required],
     descripcion: ['', Validators.required],
@@ -34,6 +40,29 @@ export class CreateCoursesComponent {
       const imagenes = cursoForm.value.imagenes;
       const categoria = cursoForm.value.categoria;
       console.log(titulo, descripcion, precio, duracion, nivel, lenguaje, imagenes, categoria);
+
+       this.product = {
+        "id": 0,  
+        "titulo": titulo,
+        "descripcion": descripcion,
+        "precio": precio,
+        "duracion": 10,
+        "nivel": nivel,
+        "lenguajes": lenguaje,
+        "imagenes": "https://www.imagenes.com/imagen.jpg",
+        "categorias": [1]
+      }
+
+      this.productsService.insert(this.product).subscribe({
+        next: () => {
+          this.toastr.success('Curso creado exitosamente');
+          this.router.navigate(['/createCourse']); 
+        },
+        error:(err) => {
+          this.toastr.error("No se ha podido crear el curso, error: " + err.message);
+        }
+      });
+      
   }
   
 }
