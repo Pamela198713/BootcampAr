2df01e086880cd4fda0dd5293f75f20ee146f8ff
LeonardoEC <LeonardoDanielAV@proton.me>
2023-06-14 23:33:07 -0300

    Arreglos del dashboard

--- frontend/src/app/pages/dashboard/dashboard.component.ts
+++ frontend/src/app/pages/dashboard/dashboard.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
 import { AuthServiceService } from 'src/app/auth/auth-service.service';
+
 
 @Component({
   selector: 'app-dashboard',
@@ -11,28 +11,34 @@ export class DashboardComponent {
   visitante: any;
   estadisticas: any;
 
-  
-  constructor(
-    private route: ActivatedRoute,
-    private authService: AuthServiceService
-    ){}
 
-  userRol:any = this.authService.getRol("1")
+  constructor(
+    private authService: AuthServiceService
+  ) { }
 
   ngOnInit(): void {
-    this.visitante = 1
-    this.estadisticas = 1
+    this.getRole()
+  }
 
-    // const userrol = this.authService.getRol('id');
-    // if(userrol === 2){
-    //   this.visitante = 2
-    // }
-    // else{
-    //   this.visitante = 1
-    // }
-
-    }
+  getRole() {
+    this.authService.getRol(1).subscribe(
+      (rol: string) => {
+        console.log(rol);
+        this.visitante = rol
+      }
+    );
+  }
+  
   
 
 
 }
+
+
+
+// if(userrol === 2){
+//   this.visitante = 2
+// }
+// else{
+//   this.visitante = 1
+// }
